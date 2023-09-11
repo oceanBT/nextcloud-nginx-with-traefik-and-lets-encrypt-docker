96ce7eb2e67cd49e0164f633a77d8cc2f651f51e
Matze <matthiaswenskus@gmail.com>
2023-09-11 19:16:40 +0000

    additional_config

---
+++ additional.config.php
@@ -0,0 +1,7 @@
+<?php
+
+// see https://docs.nextcloud.com/server/stable/admin_manual/configuration_server/config_sample_php_parameters.html
+$CONFIG = [
+    'skeletondirectory' => '',
+    'share_folder' => '/shared_with_me',
+];
