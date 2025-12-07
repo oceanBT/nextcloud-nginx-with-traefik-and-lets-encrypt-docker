1813c5b561a7f55817021d72f94fe49959920a70
ocean <ocean@wavesite.de>
2025-12-07 21:27:20 +0000

    nginx config

--- additional.config.php
+++ additional.config.php
@@ -7,4 +7,22 @@ $CONFIG = [
     'overwrite.cli.url' => $_ENV['OVERWRITEPROTOCOL'] . '://' . $_ENV['OVERWRITEHOST'] . $_ENV['OVERWRITEWEBHOST'],
     # maintenance between 1 and 5 oclock
     'maintenance_window_start' => 1,
+    'enabledPreviewProviders' => [
+'OC\Preview\Font',
+'OC\Preview\Movie',
+'OC\Preview\MSOffice2003',
+'OC\Preview\MSOffice2007',
+'OC\Preview\MSOfficeDoc',
+'OC\Preview\PDF',
+'OC\Preview\Photoshop',
+'OC\Preview\StarOffice',
+'OC\Preview\SVG',
+'OC\Preview\BMP',
+'OC\Preview\GIF',
+'OC\Preview\JPEG',
+'OC\Preview\MarkDown',
+'OC\Preview\MP3',
+'OC\Preview\OpenDocument',
+'OC\Preview\PNG',
+],
 ];
